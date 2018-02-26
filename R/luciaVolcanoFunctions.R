a7c600a2214b2994309bffad30bb71171f6f2adc
drighelli <d.righelli@gmail.com>
2018-02-26 16:57:10 -0500

    bug fixed

--- R/luciaVolcanoFunctions.R
+++ R/luciaVolcanoFunctions.R
@@ -36,31 +36,31 @@ luciaVolcanoPlot <- function(res.o, positive.controls.df, prefix,
     {
         with.pos.de$hit <- NA
         idxpc <- which(tolower(with.pos.de$gene) %in% tolower(pos.contr[,1]))
-        if(idxpc > 0) 
+        if(length(idxpc) > 0) 
         {
             with.pos.de$hit[idxpc] <- "pctr"
         
             
         
-        sub.de <- with.pos.de[which(with.pos.de$hit=="pctr"),] 
-        sub.de$col <- "pctr"
-        sub.de$lit <- "est"
-        pos.lit <- pos.contr[pos.contr[,2]=="lit",, drop=FALSE]
-        idx.lit <- which(tolower(sub.de$gene) %in% tolower(pos.lit[,1]))
-        sub.de$lit[idx.lit] <- "lit"
-        
-        sign <- which(sub.de$significance==paste("padj <", threshold))
-        sub.de$hit[sign] <- paste("pctr <", threshold)
-        notsign <- which(sub.de$significance==paste("padj >=", threshold))
-        sub.de$hit[notsign] <- paste("pctr >=", threshold)
-        
-        sub.de <- createSignLabelsNumbers(sub.de, "hit")
-        pp <- pp + 
-            geom_point(data=sub.de, aes(x=log2FoldChange, y=minuslog10pval, 
+            sub.de <- with.pos.de[which(with.pos.de$hit=="pctr"),] 
+            sub.de$col <- "pctr"
+            sub.de$lit <- "est"
+            pos.lit <- pos.contr[pos.contr[,2]=="lit",, drop=FALSE]
+            idx.lit <- which(tolower(sub.de$gene) %in% tolower(pos.lit[,1]))
+            sub.de$lit[idx.lit] <- "lit"
+            
+            sign <- which(sub.de$significance==paste("padj <", threshold))
+            sub.de$hit[sign] <- paste("pctr <", threshold)
+            notsign <- which(sub.de$significance==paste("padj >=", threshold))
+            sub.de$hit[notsign] <- paste("pctr >=", threshold)
+            
+            sub.de <- createSignLabelsNumbers(sub.de, "hit")
+            pp <- pp + 
+                geom_point(data=sub.de, aes(x=log2FoldChange, y=minuslog10pval, 
                                 shape=hit, color=col,
                                 text=paste0("padj=", padj, "\nname=", gene)), 
                                 size=0.9) +
-            scale_color_manual(values=c("red2", "blue2", "green3"))
+                scale_color_manual(values=c("red2", "blue2", "green3"))
         }
     }
    
