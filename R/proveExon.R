64ecacd2f9110212c6bc449ea7818104a6212473
Davide Risso <dar2062@med.cornell.edu>
2018-03-01 17:23:19 -0500

    Prove exons

--- R/proveExon.R
+++ R/proveExon.R
@@ -6,6 +6,8 @@ designMatrix <- ReadDataFrameFromTsv("design/all_samples_short_names.tsv.csv")
 exonmatrix <- ReadDataFrameFromTsv(file.name.path="data/NEW_PFC_exonMatrix.txt", 
                             row.names.col=NULL)
 
+counts <- exonmatrix[,c(7:dim(exonmatrix)[2])]
+
 exonDesignMatrix <- cbind(colnames(counts), designMatrix)
 exonDesignMatrix$samples <- rownames(exonDesignMatrix)
 exonDesignMatrix$exonsamples <- colnames(counts)
@@ -36,45 +38,101 @@ colnames(y)
 
 ## annotation downloaded from ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/
 
-ncbi.L1 <- readLines("data/annotation/Mus_musculus.gene_info", n = 1)
-ncbi.colname <- unlist(strsplit(substring(ncbi.L1, 10, 234), " "))
-ncbi <- read.delim("data/annotation/Mus_musculus.gene_info", skip=1, 
-                    header=FALSE, stringsAsFactors=FALSE)
+ncbi <- read.delim("data/annotation/Mus_musculus.gene_info", comment.char = "%", 
+                    header=TRUE, stringsAsFactors=FALSE)
 
-colnames(ncbi) <- ncbi.colname
 m <- match(y$genes$GeneID, ncbi$GeneID)
 y$genes$Chr <- ncbi$chromosome[m]
 y$genes$Symbol <- ncbi$Symbol[m]
 y$genes$Strand <- NULL
 head(y$genes)
-keep <- rowSums(cpm(y) > 1) >=3
-summary(keep)
+
+library(dplyr)
+library(tidyr)
+
+long_data <- gather(exonmatrix, key = condition, value = expression, S3HC5_1:WTSD5_6)
+
+long_data %>%
+    group_by(GeneID, condition) %>%
+    summarize(total = sum(expression)) %>%
+    group_by(GeneID) %>%
+    summarize(pass = sum(total > 20) >= 5) %>%
+    filter(pass) %>%
+    pull(GeneID) -> keep_ids
+
+keep <- which(exonmatrix$GeneID %in% keep_ids)
+length(keep)
+dim(exonmatrix)
 
 y <- y[keep, , keep.lib.sizes=FALSE]
 
 y <- calcNormFactors(y)
-y$samples
+y <- estimateCommonDisp(y)
+
+head(y$pseudo.counts)
 plotMDS(y)
 
-batch <- rep(1:5, 4)
-exonDesignMatrix$gcondition<-droplevels(exonDesignMatrix$gcondition )
-design <- model.matrix(~0+exonDesignMatrix$gcondition)
+## negative controls
+library(readxl)
 
-colnames(design) <- unique(exonDesignMatrix$gcondition)
+sd.ctrls <- read_excel(path="data/controls/Additional File 4 full list of BMC genomics SD&RS2.xlsx", sheet=1)
+sd.ctrls <- sd.ctrls[order(sd.ctrls$adj.P.Val),]
+
+sd.neg.ctrls <- sd.ctrls[sd.ctrls$adj.P.Val > 0.9, ]
+
+sd.neg.ctrls <- sd.neg.ctrls$`MGI Symbol`
+sd.neg.ctrls <- sd.neg.ctrls[-which(is.na(sd.neg.ctrls))]
+
+tab <- table(y$genes$Symbol)
+single <- names(which(tab==1))
+negcon <- which(y$genes$Symbol %in% intersect(sd.neg.ctrls, single))
+length(negcon)
+
+exonDesignMatrix$gcondition<-droplevels(exonDesignMatrix$gcondition )
+
+library(RUVSeq)
+groups <- makeGroups(exonDesignMatrix$gcondition)
+ruvedSExprData <- RUVs(as.matrix(round(y$pseudo.counts)), cIdx=negcon,
+                       scIdx=groups, k=5, round=FALSE)
+
+normExprData <- ruvedSExprData$normalizedCounts
+
+plotPCA(normExprData)
+plotRLE(normExprData, outline=FALSE)
+
+design <- model.matrix(~0+exonDesignMatrix$gcondition + ruvedSExprData$W)
+
+colnames(design) <- c(levels(exonDesignMatrix$gcondition), paste0("W", 1:5))
 
 y <- estimateDisp(y, design, robust=TRUE)
 y$common.dispersion
 plotBCV(y)
 
 
-fit <- glmQLFit(y, design, robust=TRUE)
-plotQLDisp(fit)
+fit <- glmFit(y, design)
 
 fit$design
 
 
 contr <- limma::makeContrasts(contrasts="KOSD5 - WTSD5", levels=design)
-qlf <- glmQLFTest(fit, contrast=contr)
+qlf <- glmLRT(fit, contrast=contr)
+topTags(qlf, p.value=0.05, n=20)
 
 # tt <- topTags(qlf,)
 # sum( tt$table$FDR < 0.05 )
+
+sp <- diffSpliceDGE(fit, contrast=contr, geneid="GeneID", exonid="Start")
+topSpliceDGE(sp, test="Simes", FDR=0.05)
+
+plotSpliceDGE(sp, geneid="Shank3", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Cntn1", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Rpl3", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Tuba1b", genecol="Symbol")
+
+plotSpliceDGE(sp, geneid="Fabp7", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Hdac7", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Per3", genecol="Symbol")
+
+plotSpliceDGE(sp, geneid="Kalrn", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Sgk1", genecol="Symbol")
+plotSpliceDGE(sp, geneid="Dido1", genecol="Symbol")
